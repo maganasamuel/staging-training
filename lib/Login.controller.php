a9131766f22fd7d177d0fee61259bcfeb1ecf113
maganasamuel <maganasamuel001@gmail.com>
2021-05-17 16:45:45 +1200

    Do not use routines

--- lib/Login.controller.php
+++ lib/Login.controller.php
@@ -27,15 +27,33 @@ class LoginController extends DB {
 		//variables
 		$emailAddress = $this->clean($emailAddress);
 		$password = $this->clean($password);
-		
+
 		//prepare/execute
-        $query = "call ad_user_login(?, ?);";
+        $query = "SET time_zone = '+13:00'";
         $statement = $this->prepare($query);
-        $statement->bind_param("ss", 
-					$emailAddress, $password);
+        $this->execute($statement);
+
+        $query = "SELECT
+                ta_user.id_user,
+                ta_user.email_address,
+                ta_user.password,
+                ta_user.first_name,
+                ta_user.last_name,
+                ta_user.date_registered,
+                ta_user.id_user_type,
+                ta_user_type.user_type
+            FROM
+                ta_user
+                LEFT JOIN ta_user_type
+                    ON ta_user.id_user_type = ta_user_type.id_user_type
+            WHERE
+                ta_user.email_address = ? AND
+                ta_user.password = ?
+            ";
+        $statement = $this->prepare($query);
+        $statement->bind_param('ss', $emailAddress, $password);
         $dataset = $this->execute($statement);
-		//error_log("call ad_user_login('{$emailAddress}','{$password}')", 0);
-		
+
 		$data["message"] = "Invalid email address or password";
 		
         if ($dataset->num_rows > 0) {
